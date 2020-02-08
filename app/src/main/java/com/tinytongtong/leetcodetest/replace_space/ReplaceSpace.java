a00665117c5137dc30ac78a3099dd35e0b15b540
wangjianzhou <tinytongtong@163.com>
2020-02-08 15:33:54 +0800

    替换字符串中的空格

---
+++ app/src/main/java/com/tinytongtong/leetcodetest/replace_space/ReplaceSpace.java
@@ -0,0 +1,28 @@
+package com.tinytongtong.leetcodetest.replace_space;
+
+/**
+ * @Description: 替换字符串中的空格
+ * https://www.nowcoder.com/practice/4060ac7e3e404ad1a894ef3e17650423?tpId=13&tqId=11155&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+ * @Author wangjianzhou@qding.me
+ * @Date 2020-02-08 14:56
+ * @Version
+ */
+public class ReplaceSpace {
+    public static String replaceSpace(StringBuffer str) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < str.length(); i++) {
+            if (' ' == str.charAt(i)) {
+                sb.append("%20");
+            } else {
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("We Are Happy");
+        String result = replaceSpace(sb);
+        System.out.println(String.format("result:%s", result));
+    }
+}
