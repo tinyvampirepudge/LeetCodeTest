e9c9696afbae56da8b7c756f636db66cfe7fe306
wangjianzhou <tinytongtong@163.com>
2020-09-08 17:06:27 +0800

    剑指 Offer 26. 树的子结构：迭代解法

--- app/src/main/java/com/tinytongtong/leetcodetest/targetoffer/question26/SubStructureOfTree.java
+++ app/src/main/java/com/tinytongtong/leetcodetest/targetoffer/question26/SubStructureOfTree.java
@@ -1,5 +1,7 @@
 package com.tinytongtong.leetcodetest.targetoffer.question26;
 
+import java.util.Stack;
+
 /**
  * @Description: https://leetcode-cn.com/problems/shu-de-zi-jie-gou-lcof/
  * 剑指 Offer 26. 树的子结构
@@ -28,8 +30,10 @@ public class SubStructureOfTree {
     }
 
     /**
+     * 递归解法
      * B是否是A的子结构
      * 第一步，先跟B的根节点比对，如果相同，就往下找
+     * 第二步，比较子树
      *
      * @param pRoot1
      * @param pRoot2
@@ -51,6 +55,13 @@ public class SubStructureOfTree {
         return result;
     }
 
+    /**
+     * 递归解法
+     *
+     * @param pRoot1
+     * @param pRoot2
+     * @return
+     */
     private static boolean doesTree1HaveTree2(TreeNode pRoot1, TreeNode pRoot2) {
         if (pRoot2 == null) {
             return true;
@@ -62,6 +73,79 @@ public class SubStructureOfTree {
             return false;
         }
         return doesTree1HaveTree2(pRoot1.left, pRoot2.left) && doesTree1HaveTree2(pRoot1.right, pRoot2.right);
+    }
+
+    /**
+     * 迭代解法
+     *
+     * @param pRoot1
+     * @param pRoot2
+     * @return
+     */
+    private static boolean isSubStructureByIteration(TreeNode pRoot1, TreeNode pRoot2) {
+        if (pRoot1 == null || pRoot2 == null) {
+            return false;
+        }
+        boolean result = false;
+        // 树的前序遍历，需要借助Stack
+        Stack<TreeNode> stack = new Stack<>();
+        // 添加根节点
+        stack.push(pRoot2);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result = doesTree1HaveTree2ByIteration(node, pRoot2);
+            if (result) {
+                return true;
+            }
+            if (node != null) {
+                if (node.left != null) {
+                    stack.push(node.left);
+                }
+                if (node.right != null) {
+                    stack.push(node.right);
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 迭代解法
+     *
+     * @param pRoot1
+     * @param pRoot2
+     * @return
+     */
+    private static boolean doesTree1HaveTree2ByIteration(TreeNode pRoot1, TreeNode pRoot2) {
+        // 对 pRoot2 做中序遍历，同步比较 pRoot1 的节点
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+        stack1.push(pRoot1);
+        stack2.push(pRoot2);
+        while (!stack2.isEmpty()) {
+            TreeNode node1 = stack1.pop();
+            TreeNode node2 = stack2.pop();
+            if (node1.val != node2.val) {
+                return false;
+            }
+            if (node2.left != null) {
+                if (node1.left != null && node1.left.val == node2.left.val) {
+                    stack1.push(node1.left);
+                    stack2.push(node2.left);
+                } else {
+                    return false;
+                }
+            }
+            if (node2.right != null) {
+                if (node1.right != null && node1.right.val == node2.right.val) {
+                    stack1.push(node1.right);
+                    stack2.push(node2.right);
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
@@ -90,5 +174,7 @@ public class SubStructureOfTree {
 
         boolean result = isSubStructure(pRoot1, pRoot2);
         System.out.println(result);
+        boolean result1 = doesTree1HaveTree2ByIteration(pRoot1, pRoot2);
+        System.out.println(result1);
     }
 }
