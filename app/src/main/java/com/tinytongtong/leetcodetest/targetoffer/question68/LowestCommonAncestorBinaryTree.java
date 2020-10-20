bd4c8c2dbc5d88709df2d2e67836ed75a7f2980a
wangjianzhou <tinytongtong@163.com>
2020-10-20 14:56:48 +0800

    剑指 Offer 68 - II. 二叉树的最近公共祖先，bug修复

--- app/src/main/java/com/tinytongtong/leetcodetest/targetoffer/question68/LowestCommonAncestorBinaryTree.java
+++ app/src/main/java/com/tinytongtong/leetcodetest/targetoffer/question68/LowestCommonAncestorBinaryTree.java
@@ -37,17 +37,11 @@ public class LowestCommonAncestorBinaryTree {
      * @return
      */
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || p == root || q == root) {
-            return root;
-        }
+        if(root == null || root.val == p.val || root.val == q.val) return root;
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
-        if (left == null) {
-            return right;
-        }
-        if (right == null) {
-            return left;
-        }
+        if(left == null) return right;
+        if(right == null) return left;
         return root;
     }
 
