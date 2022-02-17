fb676018d7b3ec059fe7f37f0519ddf66d1fee4e
wangjianzhou <tinytongtong@163.com>
2022-02-17 08:59:08 +0800

    swap test

---
+++ app/src/main/java/com/tinytongtong/leetcodetest/classify/sort/SwapTest.java
@@ -0,0 +1,20 @@
+package com.tinytongtong.leetcodetest.classify.sort;
+
+/**
+ * @Description: swap交换小技巧
+ * @Author wangjianzhou@didichuxing.me
+ * @Date 2/17/22 8:55 AM
+ * @Version
+ */
+public class SwapTest {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 12;
+        a ^= b;
+        b ^= a;
+        a ^= b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
